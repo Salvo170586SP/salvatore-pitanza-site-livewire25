b7badba253498eed5236f907ba5f9adb84fe8fe7
SalvoSalvo170586SP <pitanza.salvatore.86@gmail.com>
2025-09-29 14:03:26 +0200

    fix url bottoni

--- resources/views/livewire/admin/dashboard.blade.php
+++ resources/views/livewire/admin/dashboard.blade.php
@@ -5,27 +5,27 @@
     </div>
 
     <div class="py-5 flex flex-wrap gap-5">
-        <button
+        <button wire:navigate href="/admin/biography-home"
             class="bg-zinc-100/70 hover:bg-zinc-200/70 border border-zinc-200 dark:border-0 text-zinc-700 dark:bg-zinc-300/20 dark:hover:bg-zinc-600/70 px-4 py-3 rounded-lg dark:text-white cursor-pointer  w-[290px] h-[60px] text-lg font-semibold flex justify-between items-center">
             Biografia
         </button>
-        <button
+        <button wire:navigate href="/admin/trainings-home"
             class="bg-zinc-100/70 hover:bg-zinc-200/70 border border-zinc-200 dark:border-0 text-zinc-700 dark:bg-zinc-300/20 dark:hover:bg-zinc-600/70 px-4 py-3 rounded-lg dark:text-white cursor-pointer  w-[290px] h-[60px] text-lg font-semibold flex justify-between items-center">
             Percorsi
         </button>
-        <button
+        <button wire:navigate href="/admin/projects-home"
             class="bg-zinc-100/70 hover:bg-zinc-200/70 border border-zinc-200 dark:border-0 text-zinc-700 dark:bg-zinc-300/20 dark:hover:bg-zinc-600/70 px-4 py-3 rounded-lg dark:text-white cursor-pointer  w-[290px] h-[60px] text-lg font-semibold flex justify-between items-center">
             Progetti
         </button>
-        <button
+        <button wire:navigate href="/admin/experiences-home"
             class="bg-zinc-100/70 hover:bg-zinc-200/70 border border-zinc-200 dark:border-0 text-zinc-700 dark:bg-zinc-300/20 dark:hover:bg-zinc-600/70 px-4 py-3 rounded-lg dark:text-white cursor-pointer  w-[290px] h-[60px] text-lg font-semibold flex justify-between items-center">
             Esperienze
         </button>
-        <button
+        <button wire:navigate href="/admin/skills-home"
             class="bg-zinc-100/70 hover:bg-zinc-200/70 border border-zinc-200 dark:border-0 text-zinc-700 dark:bg-zinc-300/20 dark:hover:bg-zinc-600/70 px-4 py-3 rounded-lg dark:text-white cursor-pointer  w-[290px] h-[60px] text-lg font-semibold flex justify-between items-center">
             Skills
         </button>
-        <button
+        <button wire:navigate href="/admin/drawings-home"
             class="bg-zinc-100/70 hover:bg-zinc-200/70 border border-zinc-200 dark:border-0 text-zinc-700 dark:bg-zinc-300/20 dark:hover:bg-zinc-600/70 px-4 py-3 rounded-lg dark:text-white cursor-pointer  w-[290px] h-[60px] text-lg font-semibold flex justify-between items-center">
             Disegni
         </button>
